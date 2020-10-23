a2bb5f0e5ff20da0ffa37b7248eb3d4a1353a947
Joseph <56702275+joeguy57@users.noreply.github.com>
2020-10-23 11:06:03 -0600

    Edited JavaScript:
    Created Const

---
+++ JavaScript/brain.js
@@ -0,0 +1,11 @@
+//Variables for the score board
+const userScore = 0;
+const compScore = 0;
+const userScoreSp = document.getElementById("userScore");
+const compScoreSp = document.getElementById("compScore");
+const scoreBoardDiv = document.querySelector(".score");
+const outcomeDiv = document.querySelector(".outcome");
+//Variables for the buttons
+const rockDiv = document.getElementById("rock");
+const paperDiv = document.getElementById("paper");
+const scissorsDiv = document.getElementById("scissors");
